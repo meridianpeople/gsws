c02787ab007c8a5de6ad256406b5831c7ba0e296
GeiG Dev <dev@geig.co.uk>
2026-06-14 19:22:39 +0200

    rename: sidebar 'Web CLI' -> 'Terminal'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import './globals.css'
 import { ThemeProvider } from '@/components/layout/ThemeProvider'
 
@@ -12,6 +13,25 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" data-theme="dark">
       <body className="antialiased">
         <ThemeProvider>{children}</ThemeProvider>
+        <Script id="tawk-to" strategy="afterInteractive">
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            Tawk_API.customStyle = {
+              visibility: {
+                desktop: { position: 'bl', xOffset: 20, yOffset: 20 },
+                mobile: { position: 'bl', xOffset: 10, yOffset: 10 }
+              }
+            };
+            (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/69fcae999187a01c34e59443/1jo1gk2a8';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </Script>
       </body>
     </html>
   )
